c131c3bb8ee895498c95d89e1c95739d8e4d7dc8
Ionut Staicu <github@iamntz.com>
2014-06-26 14:58:03 +0300

    tweaked sprite generator

--- Gruntfile.js
+++ Gruntfile.js
@@ -76,15 +76,45 @@ module.exports = function(grunt) {
   });
 
 
-  var defaultSpriteOptions = {
-    algorithm  : 'binary-tree',
-    padding    : 10,
-    engine     : 'auto',
-    cssTemplate: 'helpers/spritesmith.sass.template.mustache',
-    engineOpts : {
-      'imagemagick': true
-    }
-  };
+  function get_sprites_config( sprite_group_name, options ) {
+    options = options || {};
+
+    return extend({
+      algorithm  : 'binary-tree',
+      padding    : 10,
+      engine     : 'auto',
+      cssTemplate: 'helpers/spritesmith.sass.template.mustache',
+
+      engineOpts : {
+        'imagemagick': true
+      },
+
+      src        : [ 'src/images/sprites/' + sprite_group_name + '/*.png' ],
+      destImg    : 'dist/images/' + sprite_group_name + '.png',
+      imgPath    : '../images/' + sprite_group_name + '.png',
+      destCSS    : 'src/stylesheets/sprites/_' + sprite_group_name + '.scss',
+
+      cssVarMap : function(sprite){
+        sprite.sprite_name = sprite.name;
+        if( options.hasHover &&  sprite.sprite_name.indexOf( '-hover' ) > -1  ){
+          sprite.sprite_name = sprite.sprite_name.replace('-hover', '');
+
+          if( typeof options.hasHover == 'string' ){
+            sprite.skip_if_has_hover_on_parent = true;
+            sprite.sprite_name = options.hasHover + ':hover .' + sprite_group_name + '.' + sprite.sprite_name;
+          }else {
+            sprite.sprite_name += ':hover';
+          }
+        }
+      },
+
+      cssOpts : {
+        "baseClass" : '' + sprite_group_name + '',
+        "hasHover?" : options.hasHover,
+        "functions" : true
+      }
+    }, options);
+  }//get_sprites_config
 
 
   grunt.initConfig({
@@ -148,17 +178,10 @@ module.exports = function(grunt) {
 
 
     sprite:{
-      spr: extend( defaultSpriteOptions, {
-        src        : [ 'src/images/sprites/spr/*.png' ],
-        destImg    : 'dist/images/spr.png',
-        imgPath    : '../images/spr.png',
-        destCSS    : 'src/stylesheets/sprites/_spr.scss',
-
-        cssOpts : {
-          "baseClass" : "spr",
-          "functions" : true
-        },
-      })
+      widgets: get_sprites_config( 'widgets_spr', {
+        hasHover : '.widget'
+      } ),
+      spr    : get_sprites_config( 'spr' ),
     },
 
 
